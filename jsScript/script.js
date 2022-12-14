d09f1d41108ad40a0f7c15296daec7c51fa6b274
alkatsalex <alkatsalex@ukr.net>
2022-12-14 02:01:15 +0200

    done

--- jsScript/script.js
+++ jsScript/script.js
@@ -234,26 +234,52 @@ btnChangeColor.addEventListener(`click`, getRandomHexColor)
 // task 10 -----------------------------------------------
 const boxes = document.querySelector(`#boxes`)
 const inputNumber = document.querySelector(` input[type="number"]`)
-console.log(btnCreate);
+const btnCreate = document.querySelector(`button[data-create]`)
+const btnDestroy = document.querySelector(`button[data-destroy]`)
+// console.log(btnCreate);
 let martkup = ""
 
-function getRandomHexColor() {
+function getRandomHexColorBox() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
 
-function createBoxes(amount){
-
-}
-
-for (let i = 2; i < 2 ; i+=1) {
+function createBoxes(amount) {
+  amount = inputNumber.value
+  console.log(amount);
+for (let i = 0; i < amount ; i+=1) {
   console.log(i * 10)
   const step = i * 10
   const box = document.createElement(`div`)
   boxes.append(box)
-  box.style.width = `${step + 10}px`
-  box.style.height = `${step + 10}px`
-  box.style.backgroundColor = getRandomHexColor()
+  box.classList.add(`box`)
+  box.style.width = `${step + 30}px`
+  box.style.height = `${step + 30}px`
+  box.style.backgroundColor = getRandomHexColorBox()
 }
+}
+
+
+function destroyBoxes() {
+
+  const boxes = document.querySelectorAll(`.box`)
+for (let i = 0; i < boxes.length; i +=1) {
+boxes[i].remove()
+}
+}
+
+btnCreate.addEventListener(`click`, createBoxes)
+btnDestroy.addEventListener(`click`, destroyBoxes)
+
+
+// for (let i = 2; i < 2 + 5 ; i+=1) {
+//   console.log(i * 10)
+//   const step = i * 10
+//   const box = document.createElement(`div`)
+//   boxes.append(box)
+//   box.style.width = `${step + 10}px`
+//   box.style.height = `${step + 10}px`
+//   box.style.backgroundColor = getRandomHexColorBox()
+// }
