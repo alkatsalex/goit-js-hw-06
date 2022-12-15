9ab11fab374ed98986638e9d73fed685e4a07685
alkatsalex <alkatsalex@ukr.net>
2022-12-15 16:46:02 +0200

    task2 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,19 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
 const boxes = document.querySelector(`#boxes`)
 const inputNumber = document.querySelector(` input[type="number"]`)
 const btnCreate = document.querySelector(`button[data-create]`)
 const btnDestroy = document.querySelector(`button[data-destroy]`)
 // console.log(btnCreate);
-let martkup = ""
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
 // const box = document.createElement(`div`)
 //   boxes.append(box)
@@ -24,10 +20,11 @@ let martkup = ""
 
 function createBoxes(amount) {
   amount = inputNumber.value
-  const box = document.createElement(`div`)
-   box.classList.add(`box`)
-for (let i = 0; i < amount ; i+=1) {
-  const step = i * 10
+
+  for (let i = 0; i < amount ; i+=1) {
+    const step = i * 10
+    const box = document.createElement(`div`)
+  box.classList.add(`box`)
   boxes.append(box)
   box.style.width = `${step + 30}px`
   box.style.height = `${step + 30}px`
