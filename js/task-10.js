ef6f08120b4e33537f9819c253643f095fd9d908
alkatsalex <alkatsalex@ukr.net>
2023-01-05 00:37:39 +0200

    10 done

--- js/task-10.js
+++ js/task-10.js
@@ -11,34 +11,44 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-// const box = document.createElement(`div`)
-//   boxes.append(box)
-//   box.classList.add(`box`)
-//   box.style.width = `${step + 30}px`
-//   box.style.height = `${step + 30}px`
-//   box.style.backgroundColor = getRandomHexColor()
+
+const startStep = 30;
+let divs = [];
 
 function createBoxes(amount) {
   amount = inputNumber.value
-
-  for (let i = 0; i < amount ; i+=1) {
-    const step = i * 10
+  for (let i = 0; i < amount * 10; i += 10) {
+    // const box = `<div class="box" style="width:${startStep + i}px; height:${startStep + i}px; background-color:${getRandomHexColor()};"></div>`
+    // divs.push(box)
     const box = document.createElement(`div`)
-  box.classList.add(`box`)
-  boxes.append(box)
-  box.style.width = `${step + 30}px`
-  box.style.height = `${step + 30}px`
-  box.style.backgroundColor = getRandomHexColor()
+    box.classList.add(`box`)
+    box.style.width = `${startStep + i}px`
+    box.style.height = `${startStep + i}px`
+    box.style.backgroundColor = `${getRandomHexColor()}`
+    divs.push(box)
+  }
+    boxes.append(...divs)
 }
-}
+
+
+  
+ 
+// boxes.insertAdjacentHTML("beforeend", divs);
+
+
+
+
 
 
 function destroyBoxes() {
   const boxes = document.querySelectorAll(`.box`)
-for (let i = 0; i < boxes.length; i +=1) {
-boxes[i].remove()
+  for (let i = 0; i < boxes.length; i += 1) {
+    boxes[i].remove()
+    divs = []
+    inputNumber.value = 0
+  }
 }
-}
+
 
 btnCreate.addEventListener(`click`, createBoxes)
 btnDestroy.addEventListener(`click`, destroyBoxes)
